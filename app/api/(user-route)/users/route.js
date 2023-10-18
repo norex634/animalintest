2e0fd51390c793cab3a41bfe73c385419a6be65c
moule <norex.flo@gmail.com>
2023-10-19 00:20:51 +0200

    woula 10

--- app/api/(user-route)/users/route.js
+++ app/api/(user-route)/users/route.js
@@ -4,10 +4,10 @@ import { NextResponse } from "next/server";
 export async function GET(req, res) {
     try {
         const users = await prisma.user.findMany();
-        // if (!users || users.length === 0) {
-        //     return NextResponse.json({ users: [] }, { status: 200 });
-        // }
-        return NextResponse.json( users , { status: 200 });
+        if (!users || users.length === 0) {
+            return NextResponse.json({ users: [] }, { status: 200 });
+        }
+        return NextResponse.json( {users} , { status: 200 });
     } catch (error) {
         console.error("Une erreur s'est produite lors de la récupération des utilisateurs:", error);
         return NextResponse.error("Erreur lors de la récupération des utilisateurs", { status: 500 });
