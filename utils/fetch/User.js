f77f4238e55b2ce16022fe7d47e936fa8f462a6c
moule <norex.flo@gmail.com>
2023-10-19 00:00:05 +0200

    woula 2

--- utils/fetch/User.js
+++ utils/fetch/User.js
@@ -3,9 +3,7 @@ export const GetFetchUsers = async () => {
     const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users`,{
       cache: 'no-cache',
     });
-    if (!response.ok) {
-      throw new Error(`La requête a échoué avec un statut ${response.status}`);
-    }
+    
     return await response.json();
   } catch (error) {
     console.error(error);
