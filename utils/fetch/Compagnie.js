9854eea8e818b66e2d3def92d572c983e06bbd8f
moule <norex.flo@gmail.com>
2023-10-18 22:40:51 +0200

    fetch try

--- utils/fetch/Compagnie.js
+++ utils/fetch/Compagnie.js
@@ -1,17 +1,3 @@
-export const GetFetchCompagnies = async () => {
-  try {
-    const response = await fetch(`${process.env.BASE_URL}/api/compagnies`,{
-      cache: 'no-cache',
-    });
-    if (!response.ok) {
-      throw new Error(`La requête a échoué avec un statut ${response.status}`);
-    }
-    return await response.json();
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
 
 export const GetFetchCompagnie1 = async () => {
   try {
