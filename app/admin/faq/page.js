659e9ad86780146d1b51045633c9c77541e61828
moule <norex.flo@gmail.com>
2023-10-21 02:26:26 +0200

    mon lenght 0

--- app/admin/faq/page.js
+++ app/admin/faq/page.js
@@ -13,7 +13,7 @@ const page = async () => {
     <div className='w-full h-screen bg-slate-100 p-4 flex-col justify-center'>
       <h1 className='bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium text-center'>Tous les actus</h1>
       <div className=" flex w-full h-[50%] ">
-      {/* <DataTable columns={columns} data={faqs} pageval={pageval} /> */}
+      <DataTable columns={columns} data={faqs} pageval={pageval} />
 
       
       </div>
