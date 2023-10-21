28c4229501aeb7bde33fd027afcc7a7982158769
moule <norex.flo@gmail.com>
2023-10-21 18:21:08 +0200

    animal

--- app/admin/animal/@categorie/page.js
+++ app/admin/animal/@categorie/page.js
@@ -10,7 +10,7 @@ const AnimalPageCategorie = async ({}) => {
     <div className='w-full bg-slate-100 p-4'>
       <h1 className='bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium text-center'>categorie</h1>
       
-    {/* <DataTable columns={columns} data={categoriesAnimaux} pageval={pageval} /> */}
+    <DataTable columns={columns} data={categoriesAnimaux} pageval={pageval} />
     </div>
   )
 }
