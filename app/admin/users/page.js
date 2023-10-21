60fc438c9170bf5b082eeff57bf015b877ba2e1b
moule <norex.flo@gmail.com>
2023-10-21 02:13:34 +0200

    copucou petite tortrue

--- app/admin/users/page.js
+++ app/admin/users/page.js
@@ -13,7 +13,7 @@ const page = async () => {
     <div className='w-full h-screen bg-slate-100 p-4 flex-col justify-center'>
       <h1 className='bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium text-center'>Tous les users</h1>
       <div className=" flex w-full ">
-      {/* <DataTable columns={columns} data={users} pageval={pageval} /> */}
+      <DataTable columns={columns} data={users} pageval={pageval} />
         
       
       </div>
