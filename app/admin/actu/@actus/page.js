981e89953def40eaefb9f4754be21ee15461207c
moule <norex.flo@gmail.com>
2023-10-21 18:03:28 +0200

    Actus

--- app/admin/actu/@actus/page.js
+++ app/admin/actu/@actus/page.js
@@ -5,16 +5,16 @@ import { GetFetchActus } from '@/utils/fetch/Actu'
 import { GetFetchCategorieActus } from '@/utils/fetch/CategorieActu'
 
 const page = async () => {
-  const {actus} = await GetFetchActus()
+  // const {actus} = await GetFetchActus()
   const pageval = 9
-  const {categoriesActus} = await GetFetchCategorieActus()
+  // const {categoriesActus} = await GetFetchCategorieActus()
   return (
     <>
     
     <div className='w-full h-full bg-slate-100 p-4 flex-col justify-center'>
       <h1 className='bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium text-center'>Tous les actus</h1>
       <div className=" flex w-full h-[50%] ">
-      <DataTable columns={columns} data={actus} pageval={pageval} categorieactu={categoriesActus}/>
+      {/* <DataTable columns={columns} data={actus} pageval={pageval} categorieactu={categoriesActus}/> */}
 
       </div>
 
