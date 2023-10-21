e19397d321a3d32906e912510b7b1518178dce68
moule <norex.flo@gmail.com>
2023-10-21 17:41:04 +0200

    compagnie all

--- app/admin/compagnie/layout.js
+++ app/admin/compagnie/layout.js
@@ -1,10 +1,9 @@
 import SideAdmin from "@/components/partials/SideAdmin";
-import { GetFetchCompagnie1 } from '@/utils/fetch/Compagnie';
-// import React, { useEffect, useState } from 'react';
 
 
  
 const Layoutside = ({info,horaire,social} ) => {
+
   return (
     <>
       <main className="flex min-h-screen w-full">
