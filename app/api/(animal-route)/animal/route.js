fb080f5553279215f8373e6d5021f7ab675622ef
moule <norex.flo@gmail.com>
2023-11-10 05:11:12 +0100

    fetch 3

--- app/api/(animal-route)/animal/route.js
+++ app/api/(animal-route)/animal/route.js
@@ -4,17 +4,19 @@ import { NextResponse } from "next/server";
 export async function GET(req, res) {
 
     const animaux = await prisma.Animal.findMany({
+      
+      // orderBy: {
+      //   dateArrive: 'desc', // Triez par date d'arrivée de la plus récente à la plus ancienne
+      // },
       include: {
         categorie: true,
         sexe: true,
         race: true, 
         image: true,
-    },
-    orderBy: {
-        dateArrive: "desc", // Triez par date d'arrivée de la plus récente à la plus ancienne
       },
+      
     
-    // take: 3, // Récupérer seulement le nombre d'éléments spécifié par la limite
+    take: 3, // Récupérer seulement le nombre d'éléments spécifié par la limite
   });
    console.log(animaux)
 
